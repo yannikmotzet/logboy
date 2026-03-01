1318d050c70d5e06cef72a763c84836762cc6cf6
Yannik Motzet <yannik.motzet@outlook.com>
2026-03-02 00:58:41 +0100

    logboy_node: minor refactoring

--- logboy/logboy_node.py
+++ logboy/logboy_node.py
@@ -13,42 +13,21 @@ import time
 # TODO what happens if a topic joins later and record all topics is set?
 
 class BagRecorderNode(Node):
+    
     def __init__(self, storage_path=None, robot_name=None, topics=None, ros_storage_plugin='mcap'):
+        """
+        Initialize the BagRecorderNode.
+        can be configured with parameters or through the configure_recorder() method.
+        """
         super().__init__('bag_recorder_node')
 
         self.storage_path = storage_path
         self.robot_name = robot_name
         self.ros_storage_plugin = ros_storage_plugin
         self.topics = topics # topic to record, if None all topics are recorded
+
+        self.__reset_recording() # initialize recording state
         
-        self.subscriptions_rec = [] # list of active subscriptions
-
-        # os.makedirs(self.storage_path, exist_ok=True)
-
-        # if self.topics is None:
-        #     self.set_rec_topics_all
-
-        # if config_path is None:
-        #     self.declare_parameter('config_path', 'config.yaml')  # path to the YAML configuration file
-        #     config_path = self.get_parameter('config_path').get_parameter_value().string_value
-
-        # # load configuration
-        # self.get_logger().info(f"loading configuration from: {config_path}")
-        # with open(config_path, 'r') as file:
-        #     config = yaml.safe_load(file)
-        
-        # self.storage_path = os.path.expanduser(config.get('storage_path', '/tmp'))
-        # os.makedirs(self.storage_path, exist_ok=True)
-        # self.robot_name = config.get('robot_name', 'unknown')
-
-        # self.ros_storage_plugin = config.get('ros_storage_plugin', 'mcap')
-        # self.topics = config.get('topics', [])
-        # self.subscriptions_rec = []
-
-        # if config.get('record_all_topics', False):
-        #     self.set_rec_topics_all()
-
-        # self.start_recording()
 
     def __get_bag_file_path(self):
         if not self.storage_path:
@@ -57,12 +36,17 @@ class BagRecorderNode(Node):
             raise ValueError("robot_name is not set. Please provide a valid robot name.")
         current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         return os.path.join(self.storage_path, f"{self.robot_name}_{current_time}")
+    
+    def __reset_recording(self):
+        self._subscriptions = []
+        self._writer = None # close the writer by garbage collection
+        self._topic_logs = {}
 
     def __create_writer(self, bag_file_path):
-        self.writer = SequentialWriter()
+        self._writer = SequentialWriter()
         storage_options = StorageOptions(uri=bag_file_path, storage_id=self.ros_storage_plugin)
         converter_options = ConverterOptions('', '')
-        self.writer.open(storage_options, converter_options)
+        self._writer.open(storage_options, converter_options)
 
     def __create_subscriptions(self):
         for topic in self.topics:
@@ -76,7 +60,7 @@ class BagRecorderNode(Node):
                 self.get_logger().error(f"failed to subscribe to {topic_name}, unsupported message type: {topic_type}")
                 continue
 
-            self.writer.create_topic(TopicMetadata(name=topic_name, type=topic_type, serialization_format='cdr'))
+            self._writer.create_topic(TopicMetadata(name=topic_name, type=topic_type, serialization_format='cdr'))
             
             subscription = self.create_subscription(
                 msg_type,
@@ -84,18 +68,23 @@ class BagRecorderNode(Node):
                 lambda msg, topic_name=topic_name: self.__callback(msg, topic_name),
                 10
             )
-            self.subscriptions_rec.append(subscription)
+            self._subscriptions.append(subscription)
             self.get_logger().info(f"subscribed to topic: {topic_name}")
 
     def __callback(self, msg, topic_name):
-        if not hasattr(self, '_received_first_message'):
-            self._received_first_message = set()
-        
-        if topic_name not in self._received_first_message:
+
+        if topic_name not in list(self._topic_logs.keys()):
             self.get_logger().info(f"first message received on topic: {topic_name}")
-            self._received_first_message.add(topic_name)
+            self._topic_logs[topic_name] = {
+                'first_msg_time': self.get_clock().now().nanoseconds,
+                'last_msg_time': self.get_clock().now().nanoseconds,
+                'msg_count': 0
+            }
+        else:
+            self._topic_logs[topic_name]['last_msg_time'] = self.get_clock().now().nanoseconds
+            self._topic_logs[topic_name]['msg_count'] += 1
         
-        self.writer.write(topic_name, serialize_message(msg), self.get_clock().now().nanoseconds)  # write the message to the bag file
+        self._writer.write(topic_name, serialize_message(msg), self.get_clock().now().nanoseconds)  # write the message to the bag file
 
     def __get_message_type(self, topic_type):
         try:
@@ -106,9 +95,23 @@ class BagRecorderNode(Node):
         except (ImportError, AttributeError) as e:
             raise RuntimeError(f"Failed to import message type '{topic_type}': {e}")
         
+    def is_configured(self):
+        return self.storage_path is not None and self.robot_name is not None and self.topics is not None
+        
     def configure_recorder(self, config):
-        self.storage_path = config.get('storage_path', self.storage_path)
-        self.robot_name = config.get('robot_name', self.robot_name)
+        # validate config for required keys and value types
+        mandatory_keys = ['storage_path', 'robot_name', 'topic']
+        for key in mandatory_keys:
+            if key not in config or not config[key]:
+                raise ValueError(f"Mandatory key '{key}' is missing or empty in the configuration.")
+
+        self.storage_path = config.get('storage_path')
+        self.robot_name = config.get('robot_name')
+
+        self.topics = config.get('topics')
+        if self.topics is None or self.topics == []:
+            self.set_rec_topics_all()
+
         self.ros_storage_plugin = config.get('ros_storage_plugin', self.ros_storage_plugin)
         
     def discover_topics(self):
@@ -127,41 +130,41 @@ class BagRecorderNode(Node):
         self.topics = self.discover_topics()
         
     def start_recording(self):
+        if not self.is_configured():
+            raise ValueError("Recorder is not properly configured. Please provide storage_path, robot_name and topics.")
+
         try:
             bag_file_path = self.__get_bag_file_path()
         except ValueError as e:
             self.get_logger().error(str(e))
             return
         
-        if not self.topics:
-            self.get_logger().warn("no topics configured for recording. using all discovered topics.")
-            self.set_rec_topics_all()
-        if not self.topics:
-            self.get_logger().error("no topics available for recording. cannot start recording.")
-            return
-        if self.writer is not None:
+        if self._writer is not None:
             self.get_logger().warn("already recording. stopping previous recording first.")
             self.stop_recording()
         
         self.get_logger().info(f"start recording to bag file: {bag_file_path}")
+
+        self.__reset_recording() # reset state for new recording
         self.__create_writer(bag_file_path)
         self.__create_subscriptions()
 
+
     def stop_recording(self):
         self.get_logger().info("stopping recording...")
-        for subscription in self.subscriptions_rec:
+        for subscription in self._subscriptions:
             self.destroy_subscription(subscription)
-        self.subscriptions_rec = []
-        self.writer = None # close the writer by garbage collection
+
+        self.__reset_recording()
 
     def pause_recording(self):
         self.get_logger().info("pausing recording...")
-        for subscription in self.subscriptions_rec:
+        for subscription in self._subscriptions:
             self.destroy_subscription(subscription)
-        self.subscriptions_rec = []
+        self._subscriptions = []
 
     def resume_recording(self):
-        if self.subscriptions_rec:
+        if self._subscriptions:
             self.get_logger().warn("there are already active subscriptions. pause first.")
             return
         self.get_logger().info("resuming recording...")
